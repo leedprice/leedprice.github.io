04dd7c5507a00a2865a3db9eeadc27a14e035e36
deependerch <deepender07@gmail.com>
2016-12-16 15:49:13 +0530

    No commit message

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,7 +29,7 @@
                   curSymbol = "Rs.";
               }
               else if (currency == 'CAD') {
-                  curSymbol = "C$"
+                  curSymbol = "$"
                   currency = 'USD'; //by default US
               }
               else {
