f3cd06b636ecb0211e4c5752f8c59a08274c0e2b
deependerch <deepender07@gmail.com>
2017-01-25 17:47:12 +0530

    Redirect to prd

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,125 +1,151 @@
- function print_country(country_id) {
-    
-     $("#country-icon").replaceWith("<i id=\"country-icon-dynamic\" class=\"fa fa-refresh fa-pulse\"></i>");
-     $.getJSON('https://leedonline-api.usgbc.org/v1/Common/getCountriesAndStates.json', function (data) {
-         var option_str = document.getElementById(country_id);
-         option_str.innerHTML = "";
-         option_str.length = 0;
-         option_str.options[0] = new Option('--Select Country--', '');
-         option_str.selectedIndex = 0;
-         var countries = data.countries;
-         var printCountries = Object.keys(countries);
-         printCountries.forEach(function (key) {
-             option_str.options[option_str.length] = new Option(countries[key], key);
-         });
-         $("#country-icon-dynamic").replaceWith("<i id=\"country-icon\" class=\"fa fa-globe\"></i>");
-     });
- }
- $(document).ready(function () {
-     
-     var BDCIDC =  {
-         rate: "Registration",
-         designRate: "Design Review",
-         constRate: "Construction Review"};
- 
-     var OM = {rate: "Registration" }
-     
-     $("#ratingSystem").change(function(){
-        var select = $("#ratingSystem option:selected").val();
-         switch (select) {
-             case "BD+C" :
-                 setTypeInDropDown(BDCIDC);
-                 break;
-             case "ID+C" :
-                 setTypeInDropDown(BDCIDC);
-                 break;
-             case "O+M" :
-                 setTypeInDropDown(OM);
-                 break;
-             default:
-             $("#type").empty();
-             $("#type").append("<option value=\"\">--Select Rate Type--</option>");    
-               break;  
-         }
-     });
-     
-     $("#leedprice").submit(function () {
-         
-         var country = $("#country").val();
-         var ratingSystem = $("#ratingSystem").val();
-         var type = $('#type').val();
-         var givenArea = $('#givenArea').val();
+var currentEnv = new RegExp('[\?&]env=([^&#]*)').exec(window.location.href);
+currentEnv = currentEnv ?currentEnv[1] :'PRD';
+currentEnv = currentEnv.toLowerCase();
 
-         //For safari and other browsers which does not support HTML5 validation
-         var checkBrowser=safariFormValidation(this);
-         if(checkBrowser === false){
-             console.log(checkBrowser);
-             return false;
-         }
-         console.log(checkBrowser);
+var apiBaseUrl = "https://leedonline-api.usgbc.org/v1";
+
+if(currentEnv == 'dev' || currentEnv == 'stg' || currentEnv == 'qas')
+	apiBaseUrl = "https://leedonline-api-"+currentEnv+".usgbc.org/v1";
+else
+    window.location.replace("https://leed-pricing.usgbc.org/");
+
+function print_country(country_id) {
+	$("#country-icon").replaceWith("<i id=\"country-icon-dynamic\" class=\"fa fa-refresh fa-pulse\"></i>");
+	
+	$.getJSON(apiBaseUrl+'/Common/getCountriesAndStates.json', function (data) {
+		var option_str = document.getElementById(country_id);
+		option_str.innerHTML = "";
+		option_str.length = 0;
+		option_str.options[0] = new Option('--Select Country--', '');
+		option_str.selectedIndex = 0;
+		var countries = data.countries;
+		var printCountries = Object.keys(countries);
+		printCountries.forEach(function (key) {
+			option_str.options[option_str.length] = new Option(countries[key], key);
+		});
+		$("#country-icon-dynamic").replaceWith("<i id=\"country-icon\" class=\"fa fa-globe\"></i>");
+	});
+}
+
+$(document).ready(function () {
+    var BDCIDC =  {
+		rate: "Registration",
+		designRate: "Design Review",
+		constRate: "Construction Review"
+	};
+		
+	var OM = {rate: "Registration" }
+	
+	$("#ratingSystem").change(function() {
+		var select = $("#ratingSystem option:selected").val();
+		
+		switch(select) {
+			case "BD+C" :
+				setTypeInDropDown(BDCIDC);
+				break;
+			
+			case "ID+C" :
+				setTypeInDropDown(BDCIDC);
+				break;
+			
+			case "O+M" :
+				setTypeInDropDown(OM);
+				break;
+			
+			default:
+				$("#type").empty();
+				$("#type").append("<option value=\"\">--Select Rate Type--</option>");    
+				break;  
+		}
+	});
+     
+	$("#leedprice").submit(function () {
+		var country = $("#country").val();
+		var ratingSystem = $("#ratingSystem").val();
+		var type = $('#type').val();
+		var givenArea = $('#givenArea').val();
+		var unitType = $('#unitType').val();
+		
+		//For safari and other browsers which does not support HTML5 validation
+		var checkBrowser=safariFormValidation(this);
+		
+		if(checkBrowser === false){
+			console.log(checkBrowser);
+			return false;
+		}
+		
+		console.log(checkBrowser);
         
-       
-         $("#submit-calculate").replaceWith("<button id=\"submit-dynamic\" class=\"btn btn-danger\" disabled><i class=\"fa fa-spinner fa-pulse\"></i> Calculating</button>");
-      
-         setDescrpition(givenArea, type, ratingSystem);
-         $.getJSON('https://leedonline-api.usgbc.org/v1/Common/getPriceRelatedInfo.json?countryOrCurrency=' + country, function (infodata) {
-           if(infodata.data.currency != undefined){
-              var currency = infodata.data.currency;
-              var curSymbol;
-              if (currency == 'INR') {
-                 curSymbol = "₹";
-              }
-              else if (currency == 'CAD') {
-                 curSymbol = "$"
-                 currency = 'USD'; //Taking by default USD
-              }
-              else {
-                 curSymbol = "$";
-              }
-             callTogetVersionPricesInfo(infodata, curSymbol, currency, givenArea, type, ratingSystem);
-            }
-             else {
-                 $("#modalerror").modal({backdrop: "static"});
-                 $("#submit-dynamic").replaceWith("<button id=\"submit-calculate\" class=\"btn btn-primary\" type=\"submit\" name=\"submit\"><i class=\"fa fa-calculator\"></i> Calculate</button>");
-             }
-         });
-     });
-     $(".modal-close").click(function () {
-         $('#tablebody').empty();
-         $('#pArea').empty();
-         $('#pRating').empty();
-         $('#pType').empty();
-     });
- });
+        $("#submit-calculate").replaceWith("<button id=\"submit-dynamic\" class=\"btn btn-danger\" disabled><i class=\"fa fa-spinner fa-pulse\"></i> Calculating</button>");
+		
+		setDescrpition(givenArea, unitType, type, ratingSystem);
+		
+		$.getJSON(apiBaseUrl+'/Common/getPriceRelatedInfo.json?countryOrCurrency=' + country, function (infodata) {
+			if(infodata.data.currency != undefined){
+				var currency = infodata.data.currency;
+				var curSymbol;
+				
+				if (currency == 'INR') {
+					curSymbol = "₹";
+				}
+				else if (currency == 'CAD') {
+					curSymbol = "$"
+					currency = 'USD'; //Taking by default USD
+				}
+				else {
+					curSymbol = "$";
+				}
+				
+				callTogetVersionPricesInfo(infodata, curSymbol, currency, givenArea, unitType, type, ratingSystem);
+			}
+			else {
+				$("#modalerror").modal({backdrop: "static"});
+				$("#submit-dynamic").replaceWith("<button id=\"submit-calculate\" class=\"btn btn-primary\" type=\"submit\" name=\"submit\"><i class=\"fa fa-calculator\"></i> Calculate</button>");
+			}
+		});
+	});
+	
+	$(".modal-close").click(function () {
+		$('#tablebody').empty();
+		$('#pArea').empty();
+		$('#pRating').empty();
+		$('#pType').empty();
+	});
+});
 
- function setTypeInDropDown(rateType){
-    
-    $("#type").empty();
+function setTypeInDropDown(rateType){
+	$("#type").empty();
     $("#type").append("<option value=\"\">--Select Rate Type--</option>");
-     
-    Object.keys(rateType).forEach(function(key) { 
-    $("#type").append("<option value=\"" + key + "\">" + rateType[key] + "</option>")
-  });
-     
- }
+	
+	Object.keys(rateType).forEach(function(key) { 
+		$("#type").append("<option value=\"" + key + "\">" + rateType[key] + "</option>")
+	});
+}
 
- function callTogetVersionPricesInfo(infodata, curSymbol, currency, givenArea, type, ratingSystem) {
-     
-     $.getJSON('https://leedonline-api.usgbc.org/v1/LEEDPricing/getVersionPricesInfo.json?versionOrCurrency=' + currency + '&ratingSystem=' + ratingSystem + '&area=' + givenArea + '&calculate=' + type, function (pricedata) {
-         if (pricedata.payableInfo != undefined) {
-             createTotalPriceTable(infodata, pricedata, curSymbol);
-             createTaxTable(infodata, pricedata, curSymbol);
-             createGrandTotal(infodata, pricedata, curSymbol);
-             $("#modalpopup").modal({backdrop: "static"});
-         }
-         else {
-             $("#modalerror").modal({backdrop: "static"});
-         }
-         $("#submit-dynamic").replaceWith("<button id=\"submit-calculate\" class=\"btn btn-primary\" type=\"submit\" name=\"submit\"><i class=\"fa fa-calculator\"></i> Calculate</button>");
-     });
- }
+function callTogetVersionPricesInfo(infodata, curSymbol, currency, givenArea, unitType, type, ratingSystem) {
+	var data = {
+		'versionOrCurrency': currency,
+		'ratingSystem': ratingSystem,
+		'area': givenArea,
+		'unitType': unitType,
+		'calculate': type
+	};
+	
+	$.getJSON(apiBaseUrl+'/LEEDPricing/getVersionPricesInfo.json', data, function (pricedata) {
+		if (pricedata.payableInfo != undefined) {
+			createTotalPriceTable(infodata, pricedata, curSymbol);
+			createTaxTable(infodata, pricedata, curSymbol);
+			createGrandTotal(infodata, pricedata, curSymbol);
+			$("#modalpopup").modal({backdrop: "static"});
+		}
+		else {
+			$("#modalerror").modal({backdrop: "static"});
+		}
+		$("#submit-dynamic").replaceWith("<button id=\"submit-calculate\" class=\"btn btn-primary\" type=\"submit\" name=\"submit\"><i class=\"fa fa-calculator\"></i> Calculate</button>");
+	});
+}
 
- function createTotalPriceTable(infodata, pricedata, curSymbol) {
+function createTotalPriceTable(infodata, pricedata, curSymbol) {
      
      var member = pricedata.payableInfo.member;
      var nonMember = pricedata.payableInfo.nonMember;
@@ -204,7 +230,7 @@
      document.getElementById("tablebody").appendChild(grandTotalrow);
  }
 
- function setDescrpition(givenArea, type, ratingSystem) {
+ function setDescrpition(givenArea, unitType, type, ratingSystem) {
      
      var typeMap = {
          rate: "Registration",
@@ -216,7 +242,7 @@
          "ID+C": "Interior Design and Construction (ID+C)",
          "O+M": "Building Operations and Maintenance (O+M)"
      };
-     var textnode = document.createTextNode(givenArea + " sq ft");
+     var textnode = document.createTextNode(givenArea + " "+((unitType == 'IP') ?'sq ft' :'sq m'));
      document.getElementById("pArea").appendChild(textnode);
      textnode = document.createTextNode(rSystem[ratingSystem]);
      document.getElementById("pRating").appendChild(textnode);
@@ -231,7 +257,7 @@
          "country": "Please select country in the list.",
          "ratingSystem": "Please select Rating System in the list.",
          "type": "Select Project Type.",
-         "givenArea": "Please enter area in sq ft."
+         "givenArea": "Please enter area."
      };
      switch (param) {
          case "country":
@@ -320,6 +346,9 @@
          '#head4 {' +
          'font-size:20px !important;' +
          '}' +
+         '.text-right {' +
+			'text-align: right;' +
+		 '}' +
          '</style>';
      htmlToPrint += divToPrint.outerHTML;
      newWin = window.open("");
